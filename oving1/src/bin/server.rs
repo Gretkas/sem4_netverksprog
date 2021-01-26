edaa550acb0a7a5e871bc370e966e8fc863c72a5
SigmundGranaas <sigmundsgranaas@gmail.com>
2021-01-26 18:40:36 +0100

    Build a multithreaded server by following the official rust documentation

--- oving1/src/bin/server.rs
+++ oving1/src/bin/server.rs
@@ -1,11 +1,13 @@
 extern crate lib;
+use lib::ThreadPool::ThreadPool;
 use lib::SOCKET_PATH;
+use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-use std::fs;
 
 fn main() {
     let socket = SOCKET_PATH;
+    let thread_pool: ThreadPool = ThreadPool::new(4);
 
     // Bind to socket
     let stream = match TcpListener::bind(&socket) {
@@ -18,9 +20,12 @@ fn main() {
     // Iterate over clients, blocks if no client available
     for client_stream in stream.incoming() {
         let client_stream = client_stream.unwrap();
-        
-        handle_connection(client_stream);
+
+        thread_pool.excecute(|| {
+            handle_connection(client_stream);
+        })
     }
+    println!("Shutting down")
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -30,7 +35,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request = String::from_utf8_lossy(&buffer[..]);
 
-    println!("Request: {}", request);
+    //println!("Request: {}", request);
 
     //let html_contents = fs::read_to_string("index.html").unwrap();
 
@@ -48,7 +53,6 @@ fn response_html(request: &String) -> String {
     html_contents += &html_header_as_list;
 
     html_contents += &fs::read_to_string("end.html").unwrap();
-    
     format!(
         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
         html_contents.len(),
@@ -67,5 +71,5 @@ fn add_html_header_as_string(header: &String) -> String {
 
     header_as_list += "</ul>";
 
-   return header_as_list;
+    return header_as_list;
 }
---
+++ oving1/src/lib/multithreaded_tcp_server/lib.rs
@@ -0,0 +1,4 @@
+// common.rs
+pub mod ThreadPool;
+
+pub static SOCKET_PATH: &'static str = "127.0.0.1:6969";
